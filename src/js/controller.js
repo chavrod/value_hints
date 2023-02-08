ebdc6d5bc7cc9b79d8625def0c654174096d936f
Dima Chavro <dchavro@gmail.com>
2023-02-08 16:33:48 +0300

    Get search query & load results from the model

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,24 @@
 import "../style.css";
+import * as model from "./model";
+import searchView from "./views/searchView";
 
-export const sum = (a, b) => a + b;
+const controlSearchResults = async () => {
+  try {
+    // 1) Loading Spinner
+    // 2) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 3) Load search results
+    await model.loadGeneralData(query);
+    // 4) Render results
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const init = () => {
+  searchView.addHandleSearch(controlSearchResults);
+};
+
+init();
