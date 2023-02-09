ca0b9a43fd292a139a4a3f7e06e9996e36e6006a
Dima Chavro <dchavro@gmail.com>
2023-02-09 15:52:38 +0300

    Add helper fn to format large numbers

--- src/js/helpers/dataFormatter.js
+++ src/js/helpers/dataFormatter.js
@@ -7,3 +7,19 @@ export const capitaliseFirstLetter = (string, onlyFirstWord = true) => {
         .map((word) => word.replace(word[0], word[0].toUpperCase()))
         .join(" ");
 };
+
+export const formatLargeNumber = (number) => {
+  // Twelwe Zeros for Trillions
+  return Math.abs(Number(number)) >= 1.0e12
+    ? (Math.abs(Number(number)) / 1.0e12).toFixed(2) + "T"
+    : // Six Zeroes for Millions
+    Math.abs(Number(number)) >= 1.0e9
+    ? (Math.abs(Number(number)) / 1.0e9).toFixed(2) + "B"
+    : // Six Zeroes for Millions
+    Math.abs(Number(number)) >= 1.0e6
+    ? (Math.abs(Number(number)) / 1.0e6).toFixed(2) + "M"
+    : // Three Zeroes for Thousands
+    Math.abs(Number(number)) >= 1.0e3
+    ? (Math.abs(Number(number)) / 1.0e3).toFixed(2) + "K"
+    : Math.abs(Number(number));
+};
