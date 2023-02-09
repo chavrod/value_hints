321e6ef0c77ed46504f88f555d314b32f05ca872
Dima Chavro <dchavro@gmail.com>
2023-02-09 11:15:21 +0300

    Add 'name' & 'value' properties to objects

--- src/js/model.js
+++ src/js/model.js
@@ -5,38 +5,58 @@ export const state = {
 
 const createGeneralInfo = (data) => {
   return {
-    exchange: data.Exchange,
-    ticker: data.Symbol,
-    name: data.Name,
-    market: data.Country,
-    sector: data.Sector,
-    subsector: data.Industry,
-    currency: data.Currency,
-    fiscalYearEnd: data.FiscalYearEnd,
-    description: data.Description,
+    exchange: { name: "Exchange", value: data.Exchange },
+    ticker: { name: "Ticker", value: data.Symbol },
+    name: { name: "Company Name", value: data.Name },
+    sector: { name: "Sector", value: data.Sector },
+    subsector: { name: "Subsector", value: data.Industry },
+    currency: { name: "Currency", value: data.Currency },
+    fiscalYearEnd: { name: "Fiscal Year End", value: data.FiscalYearEnd },
+    description: { name: "Description", value: data.Description },
   };
 };
 
 const createCurrentPriceRatios = (data) => {
   return {
-    price: parseFloat(
-      (data.MarketCapitalization / data.SharesOutstanding).toFixed(1)
-    ),
-    marketCap: +data.MarketCapitalization,
-    sharesOutstanding: +data.SharesOutstanding,
-    priceToEarnings: parseFloat((+data.PERatio).toFixed(1)),
-    earningsPerShare: +data.EPS,
-    priceToEarningsGrowth: parseFloat((+data.PEGRatio).toFixed(1)),
-    earningsYield: parseFloat(
-      (
-        (data.EPS / (data.MarketCapitalization / data.SharesOutstanding)) *
-        100
-      ).toFixed(2)
-    ),
-    dividendPerShare: +data.DividendPerShare,
-    dividendYield: parseFloat((data.DividendYield * 100).toFixed(2)),
-    priceToBook: parseFloat((+data.PriceToBookRatio).toFixed(1)),
-    bookValue: parseFloat((+data.BookValue).toFixed(1)),
+    price: {
+      name: "Price",
+      value: parseFloat(
+        (data.MarketCapitalization / data.SharesOutstanding).toFixed(1)
+      ),
+    },
+    marketCap: { name: "Market Cap", value: +data.MarketCapitalization },
+    sharesOutstanding: {
+      name: "Shares Outstanding",
+      value: +data.SharesOutstanding,
+    },
+    priceToEarnings: {
+      name: "P/E",
+      value: parseFloat((+data.PERatio).toFixed(1)),
+    },
+    earningsPerShare: { name: "EPS", value: +data.EPS },
+    priceToEarningsGrowth: {
+      name: "PEG",
+      value: parseFloat((+data.PEGRatio).toFixed(1)),
+    },
+    earningsYield: {
+      name: "Earnings Yield",
+      value: parseFloat(
+        (
+          (data.EPS / (data.MarketCapitalization / data.SharesOutstanding)) *
+          100
+        ).toFixed(2)
+      ),
+    },
+    dividendPerShare: { name: "DPS", value: +data.DividendPerShare },
+    dividendYield: {
+      name: "Dividend Yield",
+      value: parseFloat((data.DividendYield * 100).toFixed(2)),
+    },
+    priceToBook: {
+      name: "P/B",
+      value: parseFloat((+data.PriceToBookRatio).toFixed(1)),
+    },
+    bookValue: { name: "BPS", value: parseFloat((+data.BookValue).toFixed(1)) },
   };
 };
 
