99c536bbf5ad2dd50853b6aeab3f67a871b58b14
Dima Chavro <dchavro@gmail.com>
2023-02-10 10:25:55 +0300

    Refactor formatLargeNumber fn

--- src/js/helpers/dataFormatter.js
+++ src/js/helpers/dataFormatter.js
@@ -10,18 +10,18 @@ export const capitaliseFirstLetter = (string, onlyFirstWord = true) => {
 
 export const formatLargeNumber = (number) => {
   // Twelwe Zeros for Trillions
-  return Math.abs(Number(number)) >= 1.0e12
-    ? (Math.abs(Number(number)) / 1.0e12).toFixed(2) + "T"
+  return number >= 1.0e12
+    ? (number / 1.0e12).toFixed(2) + "T"
     : // Six Zeroes for Millions
-    Math.abs(Number(number)) >= 1.0e9
-    ? (Math.abs(Number(number)) / 1.0e9).toFixed(2) + "B"
+    number >= 1.0e9
+    ? (number / 1.0e9).toFixed(2) + "B"
     : // Six Zeroes for Millions
-    Math.abs(Number(number)) >= 1.0e6
-    ? (Math.abs(Number(number)) / 1.0e6).toFixed(2) + "M"
+    number >= 1.0e6
+    ? (number / 1.0e6).toFixed(2) + "M"
     : // Three Zeroes for Thousands
-    Math.abs(Number(number)) >= 1.0e3
-    ? (Math.abs(Number(number)) / 1.0e3).toFixed(2) + "K"
-    : Math.abs(Number(number));
+    number >= 1.0e3
+    ? (number / 1.0e3).toFixed(2) + "K"
+    : number;
 };
 
 export const appendSign = (number, sign, append = true) => {
