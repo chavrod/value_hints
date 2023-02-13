ad0b89a98fd642e61b4cf95e7014ab9dceac2cb8
Dima Chavro <dchavro@gmail.com>
2023-02-13 15:34:23 +0300

    Add bpsArr tests

--- tests/perShareCalc.test.js
+++ tests/perShareCalc.test.js
@@ -1,6 +1,6 @@
 import * as perShareCalc from "../src/js/helpers/perShareCalc";
 
-describe("calcaute historic EPS, rounding to 2 decimals", () => {
+describe("calcaute EPS for each arr position, rounding to 2 decimals", () => {
   test("Case 1 -> postive eps", () => {
     expect(perShareCalc.epsArr([10, 20], [5, 5], [1000, 1000])).toEqual([
       0.01, 0.01,
@@ -11,5 +11,15 @@ describe("calcaute historic EPS, rounding to 2 decimals", () => {
     expect(perShareCalc.epsArr([-10, -20], [5, 5], [1000, 1000])).toEqual([
       -0.01, -0.03,
     ]);
+  });
+});
+
+describe("calcaute BPS for each arr position, rounding to 2 decimals", () => {
+  test("Case 1 -> postive BPS", () => {
+    expect(perShareCalc.bpsArr([50, 100], [200, 200])).toEqual([0.25, 0.5]);
+  });
+
+  test("Case 2 -> Negative BPS", () => {
+    expect(perShareCalc.bpsArr([-50, -100], [200, 200])).toEqual([-0.25, -0.5]);
   });
 });
