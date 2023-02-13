a2e24b4237b5b98aa1899b266a4831426d7fed88
Dima Chavro <dchavro@gmail.com>
2023-02-13 15:52:30 +0300

    Add tests for tbvpsArr fn

--- tests/perShareCalc.test.js
+++ tests/perShareCalc.test.js
@@ -23,3 +23,17 @@ describe("calcaute BPS for each arr position, rounding to 2 decimals", () => {
     expect(perShareCalc.bpsArr([-50, -100], [200, 200])).toEqual([-0.25, -0.5]);
   });
 });
+
+describe("calcaute TBVPS for each arr position, rounding to 2 decimals", () => {
+  test("Case 1 -> postive TBVPS", () => {
+    expect(perShareCalc.tbvpsArr([200, 150], [50, 50], [1000, 1000])).toEqual([
+      0.15, 0.1,
+    ]);
+  });
+
+  test("Case 2 -> Negative TBVPS", () => {
+    expect(perShareCalc.tbvpsArr([100, 50], [150, 150], [1000, 1000])).toEqual([
+      -0.05, -0.1,
+    ]);
+  });
+});
