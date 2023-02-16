20a325e4a96aaf68d33a8ea2658233c0929fed6d
Dima Chavro <dchavro@gmail.com>
2023-02-16 12:49:21 +0300

    call fns to create Ratio tables

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import "../style.css";
 import * as model from "./model";
 import searchView from "./views/searchView";
 import generalTableView from "./views/generalTableView";
+import historicTableView from "./views/historicTableView";
 
 const controlSearchResults = async () => {
   try {
@@ -22,6 +23,10 @@ const controlSearchResults = async () => {
     // 6) Render results
     generalTableView.render(model.state.currentPriceRatios);
     generalTableView.render(model.state.generalInfo);
+
+    historicTableView.render(model.state.yearlyRatios.perShareRelated);
+    historicTableView.render(model.state.yearlyRatios.returnRelated);
+    historicTableView.render(model.state.yearlyRatios.debtRelated);
   } catch (err) {
     console.log(err);
   }
